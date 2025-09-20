4b7ba2c2be16512698cbae85e84cf0d842326b39
ShineBrightMeow <142170650+ShineBrightMeow@users.noreply.github.com>
2025-09-20 08:20:50 -0700

    Revert "reserve positional packet (#471)"
    This reverts commit 1ddbcd48559f40ddf2acb4a4ef05f5c01035d4c9.

--- src/network/packets.h
+++ src/network/packets.h
@@ -54,7 +54,6 @@ enum class SendPacketType : uint8_t {
 	// RotationAcceleration = 23,
 	AcknowledgeConfigChange = 24,
 	FlexData = 26,
-	// PositionData = 27,
 	Bundle = 100,
 	Inspection = 105,
 };
